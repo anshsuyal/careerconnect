8ea042672f49ffd7218991b09f92ed8e4fbc32bc
ansh sharma <anshsuyal835@gmail.com>
2026-07-03 09:39:43 +0530

    update

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -2,10 +2,10 @@ import React, { createContext } from 'react'
 import { io } from 'socket.io-client'
 
 export const authDataContext=createContext()
-export const socket = io(import.meta.env.VITE_BACKEND_URL)
+export const socket = io(import.meta.env.VITE_BACKEND_URL || (import.meta.env.DEV ? "http://localhost:8000" : ""))
 
 function AuthContext({children}) {
-const serverUrl=import.meta.env.VITE_BACKEND_URL
+const serverUrl=import.meta.env.VITE_BACKEND_URL || (import.meta.env.DEV ? "http://localhost:8000" : "")
     let value={
         serverUrl
     }
